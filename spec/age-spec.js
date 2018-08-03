470d633947ea7546f04a9b37337406be1202734a
Annie Shin <annieyshin@gmail.com>
2018-08-03 16:07:06 -0700

    give up on this one and build new one

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -4,11 +4,6 @@ describe('Age', function() {
   it('should return the age of a person in years by inputted birthdate', function() {
     let newAge = new Age("08/08/1990");
     expect(newAge.ageConverter()).toEqual(27);
-  })
+  });
 
-    // it('should return the age of a person in seconds by inputted birthdate', function() {
-    //     let newAge = new Age(08/08/2008);
-    //     expect(newWord.countVowels()).toEqual(2);
-    //   })
-
-})
+});
--- src/age.js
+++ src/age.js
@@ -4,20 +4,20 @@ class Age {
   }
 
   ageConverter() {
+    let minutes = 1000 * 60;
+    let hours = minutes * 60;
+    let days = hours * 24;
+    let years = days * 365;
+
     let date = new Date();
     let endDate = date.getTime();
-
-
-    return date;
+    let ageCalculation = Math.round(endDate / years);
+    return ageCalculation;
 
   }
 
-  // ageYearCalculator() {
-  //   let birthdate = this.birthdate;
-  //
-  //
-  // }
+
 
 }
 
-export { Age }
+export { Age };
